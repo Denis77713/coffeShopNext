212785bc6273facba0e8abbe4653c166094e9dd8
Denis77713 <demigot2033@gmail.com>
2024-08-29 20:02:06 +0600

    feat: add interactive checkbox filters

--- src/features/FilterItem/ui/FilterItem.tsx
+++ src/features/FilterItem/ui/FilterItem.tsx
@@ -6,29 +6,35 @@ import { Filter } from "@/widges/FilterList/ui/FilterList"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 
 const FilterItem: FC<{ item: Filter }> = ({ item }) => {
-
-  const searchParams:any = useSearchParams()
+  const searchParams: any = useSearchParams()
   const pathName = usePathname()
   const { replace } = useRouter()
-  const [state, setstate] = useState(true);
+  const params = new URLSearchParams(searchParams)
+  const res = params.has(item.name)
+
+  const [state, setstate] = useState(res)
+  
   return (
     <div className={style.filterItem} key={item.id}>
-      <input className={style.filterInner} type="checkbox" onChange={() => handleChange(item)} />
+      <input
+        className={style.filterInner}
+        type="checkbox"
+        onChange={() => handleChange(item)}
+        checked  = {state}
+      />
       <p className={style.filterInner}>{item.text}</p>
       {!isNaN(Number(item.value)) && <p>{item.value}</p>}
     </div>
   )
   function handleChange(item: Filter) {
     const params = new URLSearchParams(searchParams)
-    console.log(item.name)
-    if (state) {
+    if (state === false) {
       params.set(item.name, item.value)
     } else {
       params.delete(item.name)
     }
     replace(`${pathName}?${params.toString()}`)
     setstate(!state)
-
   }
 }
 
