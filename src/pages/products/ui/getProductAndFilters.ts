a713a41ab4302961ce6ab37132c7b0e93303921a
Denis77713 <demigot2033@gmail.com>
2024-08-29 17:46:08 +0600

    fix: getProductAndFilters add functions and types

--- src/pages/products/ui/getProductAndFilters.ts
+++ src/pages/products/ui/getProductAndFilters.ts
@@ -1,64 +1,54 @@
 import { prisma } from "../../../../prisma/prisma-client"
 
+export type category = { id: number; name: string; image: string; page: string }
+
 export const getCategory = async (page: string, query: any) => {
   await new Promise((resolve) => setTimeout(resolve, 1))
-  // Получить страницу
+  // Получить категорию
+  const category = await getCategoryProdusct(page)
+  // Получить товары учитывая фильтры
+  const productData = await getFilter(query, category)
+  // Получить фильтры
+  const filtersData = await getFilters(category)
+  // Вернуть Товары и фильтры
+  return { productData, filtersData }
+}
+
+async function getFilter(query: any, category: category[]) {
+  let result
+  const newQery = query
+  let search = ""
+  if (newQery.hasOwnProperty("query") === true) {
+    search = newQery.query
+    delete newQery.query
+  }
+  result = await prisma.product.findMany({
+    where: {
+      ...newQery,
+      categoryId: category[0].id,
+      name: {
+        contains: search,
+        mode: "insensitive",
+      },
+    },
+  })
+  return result
+}
+
+async function getCategoryProdusct(page: string) {
   const category = await prisma.category.findMany({
     where: {
       page: page,
     },
   })
-  let res = []
-  if (Object.keys(query).length > 0)
-    res.push(...(await getFilter(query, category)))
-  let productData
-  if (res.length === 0) {
-    productData = await prisma.product.findMany({
-      where: {
-        categoryId: category[0].id,
-        name: {
-          contains: query.query,
-          mode: "insensitive",
-        },
-      },
-    })
-  } else {
-    if (typeof query.query === "undefined") {
-      productData = res
-    } else {
-      let result: any = []
-      res.forEach((item) => {
-        if (
-          item.name
-            .toLocaleLowerCase()
-            .includes(query.query.toLocaleLowerCase()) === true
-        ) {
-          result.push(item)
-        }
-      })
-      productData = result
-    }
-  }
-  // Получить фильтры
-  const filtersData = await prisma.filter.findMany({
+  return category
+}
+
+async function getFilters(category:category[]) {
+  const filtersData = prisma.filter.findMany({
     where: {
       filterId: category[0].id,
     },
   })
-  // Вернуть Товары и фильтры
-
-  return { productData, filtersData }
-}
-
-async function getFilter(query: any, category: any) {
-  let result
-  const newQery = query
-  result = await prisma.product.findMany({
-    where: {...newQery,
-    name: {
-      contains: newQery.query,
-      mode: "insensitive",
-    }},
-  })
-  return result
+  return filtersData
 }
