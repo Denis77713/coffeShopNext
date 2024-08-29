e83c4facf48a0ff55369eff568f451e36bc1df70
Denis77713 <demigot2033@gmail.com>
2024-08-29 18:37:22 +0600

    feat: add none favorites product

--- src/widges/FavoritesNumProduct/ui/FavoritesNumProduct.tsx
+++ src/widges/FavoritesNumProduct/ui/FavoritesNumProduct.tsx
@@ -2,8 +2,7 @@
 
 import Button from "@/shared/ui/Button"
 import { useSelector } from "react-redux"
-import style from './FavoritesNumProduct.module.css'
-
+import style from "./FavoritesNumProduct.module.css"
 
 const FavoritesNumProduct = () => {
   const data = useSelector((store: any) => store.product.product)
@@ -13,9 +12,15 @@ const FavoritesNumProduct = () => {
   if (data > 1) text = "товара"
   if (data > 5) text = "товаров"
   return (
-    <div className={style.Info}>
-      <p>{`${data} ${text} на сумму: ${sum} руб.`}</p>
-      <Button>Купить</Button>
+    <div className={`${style.Info} ${style.relative}` }>
+      {sum !== 0 ? (
+        <>
+          <p>{`${data} ${text} на сумму: ${sum} руб.`}</p>
+          <Button>Купить</Button>
+        </>
+      ) : (
+        <h1 className={style.favoriteItem}>Нет избранных товаров</h1>
+      )}
     </div>
   )
 }
