d1e068d85ab05e799f99ebf5760d9bff4bc793fe
Denis77713 <demigot2033@gmail.com>
2025-01-16 12:58:48 +0600

    feat: add cors and cookieParser

--- server/main.js
+++ server/main.js
@@ -1,18 +1,30 @@
 import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { configDotenv } from 'dotenv'
 
 const app = express()
+const PORT = configDotenv().parsed.port || 5000
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
 
 async function main(){
-  app.use(express.json())
+  try {
 
-  app.use('/login',(reqest, resolve)=>{
-    resolve.status(200).json({
-      message:'complite!'
+    app.use('/login',(reqest, resolve)=>{
+      resolve.status(200).json({
+        message:'complite!'
+      })
     })
-  })
-
-  app.listen(5000,()=>{
-    console.log('port: 5000')
-  })
+  
+    app.listen(PORT,()=>{
+      console.log('port: 5000')
+    })  
+  } catch (e) {
+    console.log(e)
+  }
+  
 }
 main()
